e95b3eb1025e36b3aec7dd71410a396e395b62bc
Anton Iljina <anton.iljina@outlook.com>
2026-03-15 16:27:22 +0200

    Add full name to intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <section className="px-6 py-16 max-w-4xl mx-auto">
       <h1 className="text-3xl sm:text-4xl font-medium tracking-tight mb-4">
-        Hello! I&apos;m {introData.name.split(" ")[0]}
+        Hello! I&apos;m {introData.name.split(" ")[0]} Iljina
       </h1>
       <p className="text-muted leading-relaxed max-w-2xl">
         {introData.bio}
